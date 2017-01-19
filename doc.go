0362c72479ea5b12f4885b6afe376b55f1a094a4
lichao115 <lichao12@jd.com>
2017-01-19 14:39:33 +0800

    fix doc

--- doc.go
+++ doc.go
@@ -1,40 +1,11 @@
-// Copyright 2017-present Bryce. All rights reserved.
+// Copyright 2017-present Cyeam. All rights reserved.
 // Use of this source code is governed by a MIT-license.
 // See http://olivere.mit-license.org/license.txt for details.
 
 /*
-swiftype v1.0
-========
-
 This is a Golang client for swiftype. Only support search.
 
-### doc
-
-[godoc](https://godoc.org/github.com/mnhkahn/swiftype)
-
-### example
-
-	import "github.com/mnhkahn/swiftype"
-
-	var (
-		SWIFTYPE        *swiftype.Client
-		SWIFTYPE_APIKEY = "YOUR OWN API KEY"
-		SWIFTYPE_HOST   = "api.swiftype.com"
-		SWIFTYPE_ENGINE = "YOUR OWN ENGINE"
-	)
-
-	func InitSwiftype() error {
-		SWIFTYPE = swiftype.NewClientWithApiKey(SWIFTYPE_APIKEY, SWIFTYPE_HOST)
-		return nil
-	}
-
-	func Search(q string) *swiftype.SwiftypeResult {
-		data, err := SWIFTYPE.Search(SWIFTYPE_ENGINE, q)
-		if err != nil {
-			panic(err)
-		}
-		return data
-	}
+For more information, refer to the official [documentation](https://swiftype.com/documentation/quickstart)
 
 */
 package swiftype
